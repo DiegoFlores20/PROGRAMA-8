cae44af40ca285d68981bd719f168b18c828e730
DiegoFlores20 <floresdiego2006a@gmail.com>
2026-06-04 10:06:07 -0500

    commit de prueba

---
+++ app/src/main/java/com/example/programa8tarea/App.java
@@ -0,0 +1,24 @@
+package com.example.programa8tarea;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        double nota1, nota2, nota3, promedio;
+
+        System.out.print("Ingrese la primera nota: ");
+        nota1 = leer.nextDouble();
+
+        System.out.print("Ingrese la segunda nota: ");
+        nota2 = leer.nextDouble();
+
+        System.out.print("Ingrese la tercera nota: ");
+        nota3 = leer.nextDouble();
+
+        promedio = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("El promedio es: " + promedio);
+    }
+}
