ad940522ab6e34c4be40b576c84014cda855d95f
DiegoFlores20 <floresdiego2006a@gmail.com>
2026-06-04 16:26:34 -0500

    Correción del taller Snaider lozano

--- app/src/main/java/com/example/programa8tarea/App.java
+++ app/src/main/java/com/example/programa8tarea/App.java
@@ -8,7 +8,7 @@ public class App {
         Scanner leer = new Scanner(System.in);
 
         System.out.println("Algoritmo para calcular el promedio de 3 estudiantes con ciclo for");
-        System.out.println("validar las notas en un rango de 0 a 10 con ciclo while");
+        System.out.println("Validar las notas en un rango de 0 a 10 con ciclo while");
 
         for (int i = 1; i <= 3; i++) {
 
@@ -17,11 +17,26 @@ public class App {
             System.out.print("Ingrese la primera nota: ");
             double nota1 = leer.nextDouble();
 
+            while (nota1 < 0 || nota1 > 10) {
+                System.out.print("Error. Ingrese una nota entre 0 y 10: ");
+                nota1 = leer.nextDouble();
+            }
+
             System.out.print("Ingrese la segunda nota: ");
             double nota2 = leer.nextDouble();
 
+            while (nota2 < 0 || nota2 > 10) {
+                System.out.print("Error. Ingrese una nota entre 0 y 10: ");
+                nota2 = leer.nextDouble();
+            }
+
             System.out.print("Ingrese la tercera nota: ");
             double nota3 = leer.nextDouble();
+
+            while (nota3 < 0 || nota3 > 10) {
+                System.out.print("Error. Ingrese una nota entre 0 y 10: ");
+                nota3 = leer.nextDouble();
+            }
 
             double promedio = (nota1 + nota2 + nota3) / 3;
 
